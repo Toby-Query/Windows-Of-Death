3db9586a3e03d319456bce54df17d4e864fa32dc
Toby-Query <tobyquery1@gmail.com>
2024-07-05 21:17:58 +0200

    first chapter

--- src/pages/Chapter1.tsx
+++ src/pages/Chapter1.tsx
@@ -1,9 +1,277 @@
 import React from "react";
+import "./Chapter.css";
 
 function Chapter1() {
   return (
-    <div>
-      <h1>Chapter 1</h1>
+    <div className="parent">
+      <h1>Chapter 1: New Beginnings</h1>
+      <p>
+        If I were to reveal the naked truth, nobody would believe me. It would
+        be no different from standing on the summit of Mount Everest and
+        proclaiming to the world that Santa Claus is real. I might as well add
+        that he rides dragons to Olympus. I wouldn't believe myself either.
+        Nowadays, people aren't as gullible as their ancestors. Speaking the
+        unvarnished truth could land you in a mental institution at best, and at
+        worst, facing a fate of fire and stone. Yet, someone must know. Someone
+        must help.
+      </p>
+      <p>
+        It was on Monday, the 16th of January 2017, that I experienced the most
+        bizarre experience that can be thrust upon a 13-year-old boy. It was to
+        be my first day at the renowned Chibase Secondary School (CSS). The
+        night before, excitement had kept me reluctant to sleep, but alas, sleep
+        holds dominion over the weary minds of boys. And so, somehow, I
+        succumbed to slumber, only to awaken in an entirely different realm. I
+        found myself still within the confines of my own room, dispelling any
+        notion of being transported to some fantastical isekai world. If that
+        were the case, I suppose a conventional truck accident would have
+        sufficed. Ah, the undeniable truth of my otaku nature, proclaiming my
+        fervent love for all things anime.
+      </p>
+      <p>
+        As I was saying, my awakening unveiled an eerie scene. My room had
+        become inhabited by peculiar beings—women, to be more precise. The lack
+        of curtains on my window exposed me to their presence, and it just so
+        happened to coincide with that time of the month, the time when wolves
+        are supposed to be howling in response to the untainted light of the
+        perfectly lit full moon.{" "}
+      </p>
+      <p>
+        I rubbed my eyes, convinced that I must still be trapped within the
+        remnants of a dream. But no, as my vision sharpened, the figures came
+        into focus—strange women with an air of mystery surrounding them. Their
+        appearance was not what one would expect from ordinary inhabitants of
+        this world. They emanated an otherworldly aura, an ethereal beauty that
+        captivated my senses.{" "}
+      </p>
+      <p>
+        Curiosity mingled with apprehension as I observed them, trying to
+        comprehend their presence in my humble abode. Their attire was both
+        elegant and archaic, adorned with intricate patterns and vibrant colors
+        that seemed to defy the fashion trends of our time. Each woman possessed
+        an individuality that was both enchanting and unsettling, as if they had
+        stepped out of a long-forgotten storybook.
+      </p>
+      <p>
+        Yet, it was their eyes that held the greatest intrigue, mirroring the
+        beauty of the moon. Their gazes, intense and penetrating, seemed to
+        delve into the depths of my soul.{" "}
+      </p>
+      <p>
+        "He's here. They are not safe," whispered the woman by the window, her
+        silver hair cascading over her shoulders. Her face bore an expression
+        that seemed to embrace the mysteries of the night.
+      </p>
+      <p>
+        "Why, though? Why target children? What does he want from them?"
+        questioned an older-looking woman seated at the far end of my sofa, her
+        attire mirroring the other women in the room.
+      </p>
+      <p>
+        "This has never happened before. What are we to do?" added a voice,
+        belonging to a woman who remained out of my field of vision.
+      </p>
+      <p>
+        A different voice chimed in, filled with disappointment. "There is
+        nothing we can do."
+      </p>
+      <p>
+        This was the scene that greeted me as I awoke, the unfamiliar voices of
+        women permeating my room. Though I could only see two of them, I sensed
+        that there were at least four present, judging from their ongoing
+        discussion. In all my years, I had feared darkness, snakes, scorpions,
+        and various other creatures, but never had I experienced true fear until
+        that moment. If fear is to be swimming in the deepest oceanic trench,
+        then the best I’ve done is bathe in a bathtub. Today I felt fear. It was
+        as if the weight of the entire ocean pressed against my chest, rendering
+        my body immobile. When you command your body to move, and it stubbornly
+        refuses, you know it's fear. In my case, it was a combination of fear
+        and sleep paralysis, probably a defense mechanism my body activated in
+        that unsettling moment. Or perhaps, as my superstitious friends would
+        believe, it was the work of witches. Ah, my old friends, always quick to
+        attribute the unexplainable to the supernatural.
+      </p>
+      <p>
+        As you may have already noticed, I will be weaving between past and
+        present tense throughout this narrative. It is a reflection of the
+        disorienting nature of my experience, the blurred lines between reality
+        and the unknown.
+      </p>
+      <p>
+        In that crucial moment, time seemed to hang suspended, the air pregnant
+        with anticipation. I found myself teetering on the precipice of
+        understanding, yearning for answers to the myriad of questions swirling
+        within me. Who were these women? Why are they here?
+      </p>
+      <p>
+        In the realm of my imagination, I fancy myself a scientist—a luminary on
+        par with the great minds of Albert Einstein and Stephen Hawking. As a
+        scientist, my duty is to observe the world around me, to analyze, and to
+        formulate hypotheses. In that moment, as I lay amidst those
+        extraordinary women, a hypothesis took shape in my mind. I found myself
+        daring to believe in the existence of witches, beings of a high order
+        that I had never truly contemplated before.
+      </p>
+      <p>
+        Flexibility and gullibility are the difference between a scientist and a
+        wannabe. Flexibility is by conclusion; gullibility is by blind belief.
+      </p>
+      <p>
+        To be a scientist requires both flexibility and a healthy dose of
+        skepticism. Flexibility enables one to adapt and embrace new
+        possibilities, while skepticism acts as a safeguard against blind
+        belief. I had always prided myself on my capacity for flexibility, but
+        the concept of witches had remained firmly in the realm of fairy tales
+        and folklore. Now, faced with this inexplicable encounter, my scientific
+        mind grappled with the collision of my preconceived notions and the
+        extraordinary reality that unfolded before me.
+      </p>
+      <p>
+        In the stillness of my paralyzed state, a flicker of hope ignited within
+        the witch standing by the window. “Perhaps there is something that can
+        be done,” her name, Jessica, resonated in the air as the others turned
+        their attention towards her, eager to grasp onto any glimmer of
+        possibility.
+      </p>
+      <p>
+        "What do you mean, Jessica?" questioned one of the witches, her voice
+        laced with a mixture of curiosity and skepticism.
+      </p>
+      <p>
+        “The boy, he can see us, can’t you?” With measured steps, Jessica
+        approached me, her movements graceful and deliberate. There was an
+        otherworldly aura about her, as if she existed outside the realms of
+        conventional understanding. She lacked a discernible scent, and her
+        presence seemed to have no tangible influence on the environment around
+        her. As the other witches shifted into my line of sight, their gazes
+        fixed upon me, I couldn't help but feel a sense of intrigue mingled with
+        trepidation. Contrary to my initial assumptions, their stares betrayed
+        no malice nor darkness. Instead, I detected a hint of desperation,
+        quickly replaced by a glimmer of relief.
+      </p>
+      <p>
+        Drawing closer, Jessica extended her hand, gently placing it upon my
+        left ear. It was a gesture akin to a mother comforting her child, a
+        touch that conveyed both tenderness and urgency.
+      </p>
+      <p>
+        "Child," she began, her voice carrying a weight of importance, "we need
+        your hel..."
+      </p>
+      <p>
+        Before her words could fully register in my mind, I blinked, and in that
+        brief moment, the witches vanished from my sight. The sleep paralysis
+        remained, an eerie residue of their presence that clung to my
+        immobilized form.
+      </p>
+      <p>
+        Within a fleeting 30-second time frame, my alarm blared to life, jolting
+        me out of my paralyzed state. With a swift movement, I silenced the
+        intrusive sound, an annoyance that always seemed to snatch me away from
+        the climactic moments of my dreams. As I sat on the edge of my bed,
+        surrounded by darkness, I couldn't help but ponder the extraordinary
+        events that had unfolded just moments ago. Waking up to the presence of
+        witches surpassed any fears I had held before, even the dreaded
+        encounter with a snake paled in comparison.
+      </p>
+      <p>
+        But so what? I reminded myself that the god I fervently believed in was
+        far greater than a mere handful of witches. It was a lesson instilled in
+        me by my mother, a beacon of wisdom and faith. When faced with terror
+        and the absence of aid, she always taught me to remember that God held
+        the power to terrify the very thing that frightened me, and that he was
+        merely a prayer away. And so, with conviction burning within me, I knelt
+        on the floor, ready to seek solace in prayer.
+      </p>
+      <p>
+        I rose from my bed, knowing that the day ahead held its own unique set
+        of challenges. As I surveyed the disheveled state of my room, the
+        thought of making my bed seemed utterly pointless. Who cared about
+        neatly arranged sheets and meticulously polished shoes? The great men of
+        history didn't concern themselves with such trivialities. They lived and
+        made their mark on the world long before the invention of perfectly
+        tucked sheets and gleaming shoe polish, and their legacies will endure
+        long after the demise of these modern-day conveniences.{" "}
+      </p>
+      <p>
+        Besides, my morning routine was far from glamorous. Being the
+        unfortunate one among my two brothers, we shared the same bathtub for
+        our morning rituals. It was a chaotic dance of timing and negotiation,
+        where every second mattered in securing a few moments of warm water. The
+        concept of personal space was a luxury I couldn't afford.
+      </p>
+      <p>
+        And speaking of brothers, let me clarify something. When I say
+        "brothers," they may not necessarily be biological siblings. In the
+        realm of family relationships, I am utterly lost. Cousin, uncle, aunt,
+        niece, mother-in-law—all these terms are like a foreign language to me.
+        I am well-versed in the basics: sister, brother, mother, and father.
+        Beyond that, the intricacies of extended family connections are a
+        mystery to me, like the enigmatic complexities of the Greek language.
+      </p>
+      <p>
+         4 am, I wake up  Take a 20-minute bathe  Don on my new uniform  My
+        brothers wake up, 4:30 and 5:00 by age.
+      </p>
+      <p>
+        I couldn't help but appreciate the scent of new clothes that lingered in
+        the air. However, despite my fondness for that scent, I couldn't muster
+        the same enthusiasm for uniforms themselves. They represented order and
+        equality, a homogeneity that clashed with my own inclination towards
+        chaos and the advantages that inequality sometimes brought. But rules
+        were rules, and as I tightened the buttons and adjusted the collar, I
+        resigned myself to conforming to the expected standards.
+      </p>
+      <p>
+        Before I delve further into my own journey, allow me to introduce my
+        brothers. First, there is Gope, a mere month apart from me in age. He
+        often walks in the shadow of our eldest brother, Rasikhinkha. Gope has a
+        way of imitating Rasikhinkha's mannerisms, seamlessly blending into his
+        world. But despite his efforts, we remain distinct individuals, each
+        with our own unique experiences and perspectives.
+      </p>
+      <p>
+        Rasikhinkha, the eldest of us all at 17, carries an air of confidence
+        and authority. He embodies the ideals of the older sibling. While Gope
+        may strive to emulate Rasikhinkha, the truth is that our individuality
+        cannot be erased. We each have our own paths to forge, our own destinies
+        to fulfill.
+      </p>
+      <p>
+        As I stood before the expansive mirror in my mom's bathroom, I couldn't
+        resist the urge to flex, admiring my own reflection with a mixture of
+        confidence and amusement. It was a habit I had developed over time, a
+        momentary indulgence in self-assuredness. As I struck a pose, my eyes
+        wandered to the corner of the room where my mom engaged in a heated
+        discussion with my oldest brother.
+      </p>
+      <p>
+        Their voices carried through the slightly ajar door, revealing glimpses
+        of their ongoing dispute over money. It was a familiar scene, one that
+        played out regularly in our household. My mom, ever the vigilant
+        guardian of our finances, was convinced that my brother was seeking more
+        funds than he truly needed. A battle of wills ensued, as each tried to
+        outmanoeuvre the other, a game where the stakes were not only monetary
+        but also a test of patience and determination.
+      </p>
+      <p>
+        The clock struck 5:30, and as the hands ticked forward, signaling the
+        passage of time, we made our way through the kitchen door. The cool
+        morning breeze caressed my face, carrying with it a sense of
+        anticipation and possibility. In that moment, I recognized that a new
+        chapter of my life was about to unfold. Life had presented me with a
+        challenge, an opportunity for growth and self-discovery. And with each
+        step I took forward, I accepted that challenge with my head held high.
+      </p>
+      <p>
+        In the grand scheme of things, I saw myself as a player among billions,
+        competing to conquer the challenges that life threw our way. It was a
+        game where the best and brightest rose to the occasion, displaying their
+        resilience, adaptability, and unwavering determination. And in this game
+        of life, I saw myself as an SSS-rank player, a classification reserved
+        for the elite, those who excelled in tackling the obstacles that stood
+        in their path.
+      </p>
     </div>
   );
 }
