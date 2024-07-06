f7d9b7777cefc89f43afa258e6c6929cd5840eb2
Toby-Query <tobyquery1@gmail.com>
2024-07-06 18:04:06 +0200

    3rd chapter

--- src/pages/Chapter3.tsx
+++ src/pages/Chapter3.tsx
@@ -5,6 +5,196 @@ function Chapter1() {
   return (
     <div className="parent">
       <h1>Chapter 3: Some Stuff to End The Day</h1>
+      <p>
+        The truth is, most of my day had been a tedious affair, and I found
+        myself eagerly looking forward to the solace of my home, even if it
+        meant succumbing to a different kind of boredom. As I made my way back,
+        my only desire was to retreat to the refuge of my room, where I could
+        escape the monotony that had plagued me throughout the day.
+      </p>
+      <p>
+        Inevitably, my mom would inquire about how my first day at school went.
+        However, I had already decided to shut myself off from any conversation
+        regarding my less-than-stellar experiences. I planned to hide away,
+        seeking solace within the walls of my room and pretending as though the
+        day had never happened.
+      </p>
+      <p>
+        Oh, did I forget to mention the uniforms my mom had bought for us? They
+        were, to put it mildly, quite peculiar and had a knack for making us
+        look funny and, well, stupid. The details of this wardrobe blunder were
+        skipped in the previous chapter, but it wasn't without consequences. I
+        became the target of ridicule on my very first day, subjected to the
+        mocking laughter of my peers. The weight of their jests lingered, and I
+        couldn't help but wonder how the rest of the term would unfold.
+      </p>
+      <p>
+        Being the centre of attention, especially in such a derisive manner, was
+        not my preference. It made me feel uneasy, as if a swarm of imaginary
+        insects were crawling beneath my skin. It gave me what Rajesh from The
+        Big Bang Theory would call the "gibby gibbies," a sensation that left me
+        desperate to escape the spotlight and find solace in the shadows.
+      </p>
+      <p>
+        After school, I sought the guidance of my older brother, as I didn't
+        want to risk getting lost on my first day. Navigating the intricacies of
+        public transportation was a foreign concept to me, so I turned to
+        someone with more experience. I found him near Greenhouse, a local shop
+        renowned for its delicious vetkoeks, the South African equivalent of
+        doughnuts. My brother was in the company of his friends, and to my
+        surprise, he seemed genuinely kind towards me. He even offered to buy me
+        a vetkoek as a gesture of goodwill. However, I wasn't accustomed to him
+        behaving this way, so I politely declined, claiming to be already full.
+      </p>
+      <p>
+        As we arrived home, his demeanour underwent a sudden transformation. He
+        became distant and cold, and his words pierced through me like daggers.
+        "Don't ever do that again. I don't associate with kids. In fact, pretend
+        like we don't know each other at all when I'm around my friends. I don't
+        have a dumb brother." Did his words hurt? Not even a little. I had grown
+        accustomed to such harsh treatment. It was the familiar sting of his
+        words that lulled me to sleep on most nights. Paradoxically, it was his
+        previous display of kindness that sent shivers down my spine. There was
+        something unsettling about his temporary benevolence, a stark contrast
+        to the norm, and it filled me with an inexplicable sense of unease.
+      </p>
+      <p>
+        Alone in the confines of my room, where solitude reigned supreme, I
+        sought solace in the familiar ritual of introspection. It was in these
+        moments that I turned to my most trusted confidante, my guardian angel,
+        my beloved sister Lani. Grasping my phone, I dialled her number and
+        eagerly awaited her comforting voice. I poured out the events of my day,
+        recounting the encounters with the so-called witches and the unsettling
+        behaviour of my brother. And as always, she fulfilled her role as a
+        patient listener, offering her insightful commentary.
+      </p>
+      <p>
+        But deep down, did I truly believe the words of wisdom emanating from
+        the other end of the phone call? Well, I feigned conviction. How could I
+        place absolute faith in someone whose profession involved persuading
+        individuals that their perceived reality was but an illusion? Yes,
+        that's right—Lani was a psychologist, and I had been her patient long
+        before she embarked on her own journey in the field.
+      </p>
+      <p>
+        "It had to be someone. Someone had to bear the burden of hallucinations,
+        endure ridicule, and have him as a brother. Unfortunate as it may be,
+        the universe chose you. But fear not, for you are the one who possesses
+        the strength to withstand it all. With the passage of time, your
+        discomfort will fade away," reassured my sister, her words transcending
+        her role as a psychologist. Those words, spoken by my sister, held the
+        greatest solace the world could offer me. It was enough, for it emanated
+        from my source of peace, my other half, and it resonated deep within my
+        soul.
+      </p>
+      <p>
+        In various Asian cultures, there exists a belief that the gods have
+        intricately tied a sacred thread onto every living person, symbolizing
+        their connections to others. This mystical thread, known as the red
+        thread of fate, is said to be an unbreakable bond that links individuals
+        together, often in ways that transcend time, place, and circumstance.
+      </p>
+      <p>
+        The red thread is believed to be a manifestation of destiny, weaving the
+        lives of those who are meant to meet and impact each other's journeys.
+        It is said that regardless of the paths they traverse or the obstacles
+        they face, the thread will guide them towards one another, ensuring
+        their fates remain forever intertwined.
+      </p>
+      <p>
+        While the concept of the red thread of fate is commonly associated with
+        destined lovers who are believed to be bound by the thread until they
+        unite in marriage, it can also represent other profound connections. In
+        my case, I use the metaphorical comparison of the red thread to
+        illustrate the deep bond I share with my sister, Lani. Our connection
+        transcends the bounds of mere siblinghood, resembling the intertwined
+        destinies often attributed to the red thread of fate.
+      </p>
+      <p>
+        So, in embracing the belief in the red thread of fate, I find solace in
+        knowing that despite the challenges and uncertainties of life, my
+        connection to Lani remains steadfast. It is a reminder that our
+        destinies are forever intertwined, and the red thread continues to bind
+        us together in a unique and unbreakable way.
+      </p>
+      <p>
+        As I lay in bed, preparing to drift off to sleep, I began my nightly
+        routine of reciting everything I had learned throughout the week. This
+        ritual not only helped me to keep my memory sharp but also served as a
+        reminder of my own strength—the power of my mind and the knowledge that
+        propelled me towards success.
+      </p>
+      <p>
+        While it was only my first day at secondary school, I had already been
+        studying ahead during the holidays, eager to stay ahead of the curve.
+        Reciting my learnings before sleep was not just a voluntary act; it was
+        compulsory for me, a personal commitment I made to myself.
+      </p>
+      <p>
+        Before I conclude this chapter, I want to mention that the productive
+        habit approach is just one of several strategies to achieving success.
+        In subsequent chapters, I will explore other approaches and share
+        additional insights that have shaped my journey thus far.
+      </p>
+      <p>
+        For now, let me quote something I got from the Marco Polo series: “Kung
+        Fu. It means ‘supreme skill from hard work’. A great poet has reached
+        Kung Fu. The painter, the calligrapher, they can be said to have Kung
+        Fu. Even the cook, the one who sweeps steps, or a masterful servant can
+        have Kung Fu. Practice. Preparation. Endless repetition. Until your mind
+        is weary, and your bones ache. Until you’re too tired to sweat, too
+        wasted to breathe. That is the way, that is the only way one acquires
+        Kung Fu.”
+      </p>
+      <p>
+        Tell me, what have you learned today? Nothing. What have you learned
+        this week? Nothing. What have you learned this month? Nothing. What did
+        you learn yesterday? Nothing. It was gone, it was all gone. My brain had
+        performed a coup d’état. I could not remember a thing. Perhaps my day
+        was just too boring, I tell myself.
+      </p>
+      <p>
+        I lay there, waiting to wake up so I can wonder at what moment I fell
+        asleep. As I waited for the bus, a train came in its place. This is the
+        night it began, the slowly increasing intensity of sound. Imagine
+        someone drawing closer to your ear while screaming their lungs out.
+        That’s what it felt like, except that it sounded more like it was
+        hundreds of people. And unlike the scenario, you can’t snap at the
+        person or cover your ears because it’s in your head. Do you know that
+        sweet voice in your head that’s very fluent in language? I heard
+        hundreds of them, fluent in screaming and very depressing. An entire
+        horde was screaming, each wanting its voice to be heard. I decided to
+        call them the Screaming Horde.
+      </p>
+      <p>
+        At the end of the day, I reached three unsettling conclusions that left
+        me grappling with the newfound changes in my life.
+      </p>
+      <p>
+        Firstly, my hearing had taken an inexplicable leap in sensitivity. It
+        was as if a veil had been lifted, granting me the ability to perceive
+        even the faintest of sounds. While this might sound like a superpower
+        from a comic book, in reality, it felt like a weakness. The world around
+        me became a minefield of auditory bombardment, overwhelming my senses.
+        Doctors might call it Hyperacusis, a condition where everyday sounds
+        become painfully amplified.
+      </p>
+      <p>
+        Secondly, I noticed a disturbing shift in my memory. It seemed that
+        certain fragments of my life had vanished into thin air, leaving gaps in
+        my recollection. I found myself struggling to recall important details
+        and moments that were once etched into my mind. It was as if someone had
+        discreetly edited my mental archives, erasing crucial data. No, there
+        were no mysterious agents in black suits involved, just a disconcerting
+        sense of loss.
+      </p>
+      <p>
+        Lastly, and perhaps most distressingly, I discovered that even in the
+        absence of sound, silence could scream out to me for help. It was an
+        eerie paradox—a void of sound that carried an overwhelming weight, a
+        silent plea for attention. How does one silence the silence itself? The
+        question reverberated in my mind, haunting me as I searched for answers.
+      </p>
     </div>
   );
 }
