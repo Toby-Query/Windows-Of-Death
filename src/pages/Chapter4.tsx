078feb65298bfe19224b3e63bfa97eba237109d8
Toby-Query <tobyquery1@gmail.com>
2024-07-06 18:15:19 +0200

    5th chapter

--- src/pages/Chapter4.tsx
+++ src/pages/Chapter4.tsx
@@ -4,7 +4,261 @@ import "./Chapter.css";
 function Chapter1() {
   return (
     <div className="parent">
-      <h1>Chapter 4: The School System</h1>
+      <h1>Chapter 4: Matters of Religion</h1>
+      <p>
+        In the tapestry of teenage life, it is almost customary to delve into
+        matters of religion, along with sex, drugs, and depression. While my
+        personal experiences may not be extensive, I feel compelled to share my
+        thoughts on this intricate subject.
+      </p>
+      <p>
+        Do I believe in God? The answer is an affirmative one. I hold faith in
+        the existence of the one true God, the deity revered by the followers of
+        Abraham, Moses, and countless others. To me, this God is not a stagnant
+        entity but rather one that evolves alongside humanity, adapting to the
+        changing needs and understanding of its creations.
+      </p>
+      <p>
+        The foundation of my beliefs can be traced back to the influence of my
+        mother and a fellow student I met at my new school, a remarkable young
+        girl named Saint. It was through her that I gained insights into the
+        Christian faith. Saint introduced me to the concept of the Christian
+        God, who is viewed as a trinity comprising God the Father, God the Son,
+        and God the Holy Spirit. This triune nature gives rise to a distinct
+        timeline within the Christian religion, encompassing three distinct
+        parts.
+      </p>
+      <p>
+        The Old Testament marks the first timeline in the Christian faith, a
+        period governed by God the Father, an incredibly strict and deeply
+        involved deity. In deference to Saint's preference, I will use a capital
+        "G" for God.
+      </p>
+      <p>
+        Personally, I must admit that I would not find it desirable to live
+        during this era. I have always harboured reservations towards
+        individuals who exhibit excessive strictness, which may stem from a
+        profound emotional response within me. The Old Testament recounts
+        numerous events that, by conventional standards, would be deemed
+        extraordinary: the parting of seas, the destruction of entire cities,
+        and other extraordinary interventions. Such interventions were a
+        testament to God's intense involvement with humanity during this time.
+      </p>
+      <p>
+        However, as the pages turned, a shift occurred, and a new divine
+        presence emerged.
+      </p>
+      <p>
+        The New Testament ushers in an era characterized by mercy and
+        compassion, with Jesus Christ as the central figure. He is regarded as
+        the Messiah, embodying the love that God has for the world. In him, the
+        Father is revealed, and he imparts crucial messages to humanity. Above
+        all, his teachings emphasize the importance of loving God and loving
+        one's neighbours profoundly.
+      </p>
+      <p>
+        Jesus redefined many beliefs that had become burdensome and hindered the
+        progress of mankind. He emphasized that it is not what goes into a
+        person's mouth that matters, but rather what comes out of it. He
+        highlighted the remarkable power of faith, no matter how small it may
+        be. Throughout his life, Jesus fulfilled numerous prophecies, and
+        ultimately, mankind crucified God on a cross. This act became a powerful
+        symbol of God's capacity to forgive and offer second and third chances.
+      </p>
+      <p>
+        However, this reign, although brief, also came to an end. What remained
+        were two fundamental elements: the name of the Messiah and the presence
+        of our new God, God the Holy Spirit.
+      </p>
+      <p>
+        In the current era, we find ourselves in the presence of a lesser-known
+        God, yet one who governs the times we live in. This God is the spirit of
+        God, empowering humanity to rule the world, as it was originally
+        intended. The overt involvement of God in human affairs has become less
+        evident, leading to doubt and questioning.
+      </p>
+      <p>
+        Mankind has been bestowed with everything necessary to propel itself
+        forward and govern its own destiny. We have reached a point where we
+        have created a sense of unity among all people through an entirely
+        distinct religion—the religion of humanity. This religion transcends
+        specific faiths such as Christianity or Buddhism. It recognizes humanity
+        itself as the one true religion.
+      </p>
+      <p>
+        One of the fundamental principles that hold true both in heaven and on
+        earth is the significance of humanity. It forms the cornerstone of
+        objective morality, while everything else falls under the realm of
+        subjective morality. Recognizing and honouring the value of humanity is
+        at the core of our collective responsibility and moral compass.
+      </p>
+      <p>
+        Now, let us delve into the thought-provoking questions that arise when
+        considering matters of faith. Am I a Christian? In a strict definition,
+        yes, I believe in Christ. However, when it comes to identifying with a
+        specific religious label, I find myself aligning more with the broader
+        essence of humanity. I see myself as a human, embracing the
+        interconnectedness and shared experiences that unite us all.
+      </p>
+      <p>
+        Turning our attention to the symbolism of the cross, what does it
+        represent to me personally? Is it a symbol of God's love for the world,
+        a symbol of mercy, or does it hold no significance at all? To be honest,
+        the cross holds little meaning in my life. I did not have the privilege
+        of walking alongside Christ or witnessing the profound impact of his
+        sacrifice firsthand. While I recognize the deep emotional resonance it
+        carries for those who lived during that time, I cannot fully comprehend
+        or replicate their intense connection to the cross.
+      </p>
+      <p>
+        For me, the concept of God takes on a different meaning, and I yearned
+        for a symbol that encapsulates my own unique understanding of the
+        divine. Thus, I embarked on creating my own symbol for God, one that
+        resonates with my personal journey and spiritual beliefs. This symbol
+        serves as a visual representation of how I perceive and connect with the
+        divine forces that shape my existence. It allows me to express my
+        individual perspective on God, reflecting the depths of my own spiritual
+        exploration.
+      </p>
+      <p>
+        In my symbol for God, the design incorporates various elements that hold
+        personal significance to me. At the top, there is an orb representing
+        the Father, the ultimate source of divine wisdom and guidance. The
+        flowing lines connecting the orbs symbolize the continuous reign and
+        influence of God throughout different periods of history.
+      </p>
+      <p>
+        The two orbs below represent the Son and the Holy Spirit, representing
+        the divine manifestations of God's presence in the world. They serve as
+        reminders of the transformative power and grace that can be found in the
+        Christian narrative. Within the symbol, there are four short
+        perpendicular lines, each representing a different aspect of my personal
+        values in life. However, the long central line holds the utmost
+        importance as it represents the fundamental law of heaven: love. Love is
+        the timeless and unchanging essence of God's nature, the guiding
+        principle that transcends any shifting values or beliefs.
+      </p>
+      <p>
+        It is worth noting that the meaning of each of the four lines
+        representing personal values may evolve and change over time, reflecting
+        the dynamic nature of human growth and self-discovery. This aspect of
+        the symbol emphasizes my belief in a God who evolves with the changing
+        times, while upholding the eternal and unwavering love that God has for
+        all of humanity.
+      </p>
+      <p>
+        Overall, my self-created symbol for God represents a personal
+        interpretation of divinity, incorporating elements of evolution, love,
+        and the exploration of one's values in the journey of faith.
+      </p>
+      <p>
+        Regarding science, I hold a firm belief in its validity and
+        significance. Science, in my perspective, is a tool that allows humans
+        to explore and understand the natural world. It is a product of the
+        intricate workings of God's creation, providing us with a means to
+        uncover the mysteries of the universe. I share the viewpoint that God
+        does not create things multiple times but rather establishes a
+        foundation upon which scientific principles operate. Through scientific
+        inquiry, we gain insights into the laws and mechanisms that govern the
+        world around us. It is through the study of science that we are able to
+        appreciate the intricate complexity and beauty of God's creation. Like
+        the Veteran always says, God does not create twice.
+      </p>
+      <p>
+        He created things once and everything after that was pure science
+        subjected to pure nonsensical interpretations.
+      </p>
+      <p>
+        Saint, the girl I met at the YCM (Youth Church……something), made a
+        profound impact on me. The YCM was a religious gathering that took place
+        during school breaks, resembling a church-like event. Attending was a
+        matter of personal choice, whether out of a sense of obligation or a
+        genuine desire to be there. In my case, I attended because I felt it was
+        expected of me, but for Saint, it was a different story.
+      </p>
+      <p>
+        As I observed her at the front, fully immersed in praise and worship, it
+        was evident that she had a deep connection with her faith. She didn't
+        care about the eyes watching her or the judgments that might arise. It
+        was a personal communion between her and her God, a moment of pure
+        devotion and conversation.
+      </p>
+      <p>
+        In that encounter, I glimpsed the essence of Christianity through Saint.
+        She embodied the heart of the religion, showcasing unwavering faith,
+        authenticity, and an unshakeable connection with her beliefs. It was a
+        powerful reminder that religion is not just about rituals or external
+        appearances, but about the intimate relationship one cultivates with
+        their spiritual path.
+      </p>
+      <p>
+        I couldn't help but feel drawn to Saint, compelled to approach her and
+        engage in a conversation that would unravel her deep-rooted beliefs.
+        Perhaps it was my own longing to have the same level of conviction, or
+        maybe I was simply intrigued by her story.
+      </p>
+      <p>
+        As we spoke, Saint's words resonated with a profound knowledge of God
+        and scripture. She effortlessly recited verses and questioned my own
+        beliefs, challenging me to examine the foundations of my faith. Every
+        response she offered seemed meticulously crafted, as if it were a
+        perfectly rehearsed script.
+      </p>
+      <p>
+        There was a certain allure in her ability to communicate with such
+        clarity and precision. It was as if she possessed a gift for delivering
+        flawless speeches, leaving those who listened in awe of her words. I
+        admired her unwavering commitment to her beliefs, the certainty with
+        which she expressed herself. It was a reminder of the power of
+        deep-rooted faith and the impact it can have on one's speech and
+        presence.
+      </p>
+      <p>
+        In the presence of Saint, it felt as though I was engaging in a dialogue
+        straight from the pages of the Bible itself. Her unwavering devotion to
+        God's teachings was evident in every word she spoke. There was no room
+        for doubt or hesitation in her discourse. It was a remarkable quality,
+        one that commanded attention and respect.
+      </p>
+      <p>
+        I liked that about her, the idea of having perfect speech. A speech so
+        perfect that the person you are talking to will think that every reply
+        you give is premeditated, a script for a movie or so.
+      </p>
+      <p>
+        “Iron sharpens iron, and one person sharpens another.” Proverbs 27:17
+      </p>
+      <p>
+        Through my growing friendship with Saint, I discovered a treasure that
+        would become my favorite book—the Proverbs. It was a collection of
+        timeless wisdom and practical guidance that resonated deeply within me.
+        As I delved into its pages, I found solace in the profound insights and
+        life lessons it offered.
+      </p>
+      <p>
+        Within its pages, I found wisdom that transcended time and culture. The
+        Proverbs were not confined to any specific religious doctrine but spoke
+        to the universal principles that govern human existence. Each proverb
+        was like a precious gem, shining with truth and providing guidance for
+        navigating the intricacies of life.
+      </p>
+      <p>
+        With Saint's guidance, I embarked on a journey to reconnect with my
+        father, who, unfortunately, wasn't known for his conversational skills.
+      </p>
+      <p>
+        As I delved deeper into Saint's life, I discovered that she chose to be
+        a B student. Despite possessing exceptional intellect and capabilities,
+        she purposefully aimed for a lower grade class. I guess you can say she
+        pulled that Ayanokoji move.
+      </p>
+      <p>
+        Just like Ayanokoji, she had her own story to tell. A 13-year can’t be
+        that great of a Christian with no sad story behind it, as an observer,
+        it’s my duty to know every background story. I sticked around until I
+        was trusted, until I was a friend.
+      </p>
+      <p>In her words, I did my best and God did the rest.</p>
     </div>
   );
 }
--- src/pages/Chapter5.tsx
+++ src/pages/Chapter5.tsx
@@ -1,7 +1,43 @@
 import React from "react";
 
 function Chapter1() {
-  return <div>Chapter1</div>;
+  return (
+    <div className="parent">
+      <h1>Chapter 5: The School System</h1>
+      <p>
+        I have some time to kill, so let’s look at the grading system and all
+        that.
+      </p>
+      <p>
+        As a grade 8 student, I had to study nine different subjects, each with
+        its own passing requirements. But I didn't really care about the minimum
+        percentages needed to pass, I was not going get low enough to care. To
+        me it was more of a question of how much I need for level 7 and 6. To
+        those unfamiliar with levels, just assume that 7 is A and 6 is B. If you
+        are Asian, just assume 7 is D-plus, 6 is D-minus.
+      </p>
+      <p>
+        1. Mathematics: The second most challenging subject among the nine. I
+        never fully grasped the difficulty of math until I reached grade 8. I
+        naively believed that it would become easier with the introduction of
+        calculators (apologies to the Asians). I used to criticize Raskhinkha
+        for struggling with math when he had a calculator at his disposal. But
+        my days of judgment came to an abrupt halt, as I found myself sitting in
+        the defendant's seat. I, like Jesus, wept.
+      </p>
+      <p>
+        2. Tshivenda Home Language: According to Galileo Galilei, Mathematics is
+        the language in which God has written the universe. In my opinion,
+        that's because even God couldn't fathom writing the universe in my home
+        language. Allow me to introduce you to the most challenging subject
+        among the nine. It may sound like common knowledge, but it's far from
+        it. Tshivenda is only easy for those who have already reached advanced
+        years. The younger generation knows very little, and the youngest ones
+        struggle even more. It's not that the language is dying; it's just that
+        certain aspects were never necessary to begin with.
+      </p>
+    </div>
+  );
 }
 
 export default Chapter1;
